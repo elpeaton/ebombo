cf89f23f800fedaef907d587acc8128a2857bdc9
humberto delgado <dehumberto@gmail.com>
2021-01-14 18:44:59 -0500

    resize for a tablet

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  width: 375px;
+  width: 100%;
   margin: auto;
+  @media (min-width: 769px) {
+    width: 375px;
+  }
 `;
